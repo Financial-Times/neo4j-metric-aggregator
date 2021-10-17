780b70a3a9de02471a6fa97be82fe08512e17ed2
Elitsa Pavlova <elitsa.pavlova@ft.com>
2021-10-17 17:55:39 +0300

    Refactor http server and http request logging

--- go.mod
+++ go.mod
@@ -6,16 +6,18 @@ require (
 	github.com/Financial-Times/cm-neo4j-driver v0.0.1
 	github.com/Financial-Times/go-fthealth v0.0.0-20180807113633-3d8eb430d5b5
 	github.com/Financial-Times/go-logger/v2 v2.0.1
+	github.com/Financial-Times/http-handlers-go/v2 v2.3.0
 	github.com/Financial-Times/service-status-go v0.0.0-20160323111542-3f5199736a3d
 	github.com/Financial-Times/transactionid-utils-go v0.2.0
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/google/uuid v1.0.0
+	github.com/gorilla/mux v1.8.0
 	github.com/hashicorp/go-version v1.0.0 // indirect
-	github.com/husobee/vestigo v1.1.0
 	github.com/jawher/mow.cli v1.0.4
 	github.com/konsorten/go-windows-terminal-sequences v0.0.0-20180402223658-b729f2633dfe // indirect
 	github.com/neo4j/neo4j-go-driver/v4 v4.3.3 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20161128210544-1f30fe9094a5
 	github.com/sirupsen/logrus v1.1.0 // indirect
 	github.com/stretchr/objx v0.1.1 // indirect
 	github.com/stretchr/testify v1.5.1
--- handlers/concept_metrics_handler.go
+++ handlers/concept_metrics_handler.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"net"
 	"net/http"
 	"strings"
 
@@ -37,24 +36,19 @@ func (h *ConceptsMetricsHandler) GetMetrics(w http.ResponseWriter, r *http.Reque
 	uuids, err := h.extractConceptUUIDs(r)
 	if err != nil {
 		h.writeJSONError(w, err, http.StatusBadRequest)
-		h.writeRequestLog(r, tid, http.StatusBadRequest)
 		return
 	}
 
 	concepts, err := h.metricsAggregator.GetConceptMetrics(ctx, uuids)
 	if err != nil {
 		h.writeJSONError(w, err, http.StatusInternalServerError)
-		h.writeRequestLog(r, tid, http.StatusInternalServerError)
 		return
 	}
 
 	if err = json.NewEncoder(w).Encode(&concepts); err != nil {
 		h.writeJSONError(w, err, http.StatusInternalServerError)
-		h.writeRequestLog(r, tid, http.StatusInternalServerError)
 		return
 	}
-
-	h.writeRequestLog(r, tid, http.StatusOK)
 }
 
 func (h *ConceptsMetricsHandler) extractConceptUUIDs(r *http.Request) ([]string, error) {
@@ -84,42 +78,4 @@ func (h *ConceptsMetricsHandler) writeJSONError(w http.ResponseWriter, err error
 		h.log.WithError(err).Error("Failed to write json data to response")
 		return
 	}
-}
-
-func (h *ConceptsMetricsHandler) writeRequestLog(req *http.Request, transactionID string, status int) {
-	username := "-"
-	if req.URL.User != nil {
-		if name := req.URL.User.Username(); name != "" {
-			username = name
-		}
-	}
-
-	host, _, err := net.SplitHostPort(req.RemoteAddr)
-
-	if err != nil {
-		host = req.RemoteAddr
-	}
-
-	req.URL.RawQuery = ""
-	uri := req.URL.RequestURI()
-
-	// Requests using the CONNECT method over HTTP/2.0 must use
-	// the authority field (aka r.Host) to identify the target.
-	// Refer: https://httpwg.github.io/specs/rfc7540.html#CONNECT
-	if req.ProtoMajor == 2 && req.Method == "CONNECT" {
-		uri = req.Host
-	}
-
-	h.log.WithFields(map[string]interface{}{
-		"host":           host,
-		"username":       username,
-		"method":         req.Method,
-		"transaction_id": transactionID,
-		"uri":            uri,
-		"protocol":       req.Proto,
-		"status":         status,
-		"referer":        req.Referer(),
-		"userAgent":      req.UserAgent(),
-	}).Info()
-
 }
